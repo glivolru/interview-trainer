fe6677334ff54cd37d66ea22964f2ce0d49ef084
glivolru <ivanglushenkov@yandex.ru>
2026-04-06 01:45:45 +0400

    add endpoint /tasks

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"message": "Interview Trainer API"}
+
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Two Sum",
+        "difficulty": "easy"
+    },
+    {
+        "id": 2,
+        "title": "Palindrome",
+        "difficulty": "easy"
+    }
+]
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
