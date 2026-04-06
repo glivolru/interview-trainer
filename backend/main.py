b94ae88dceaa28c84aca7b9cc121560bb3766a7a
glivolru <ivanglushenkov@yandex.ru>
2026-04-06 18:30:21 +0400

    enable CORS for frontend requests

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -33,3 +34,11 @@ def get_tasks(task_id: int):
         if task["id"] == task_id:
             return task
     return {"error": "Task not found"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
