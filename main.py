befcfc90d2c19bb30a120a5fb0166b1f9070edb8
glivolru <ivanglushenkov@yandex.ru>
2026-04-06 13:16:38 +0400

    add getting a task by task_id

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ tasks = [
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_tasks(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    return {"error": "Task not found"}
